fd834f17d30d1497fb68d9c7b6e1f154c7d36052
MayceAlazzawi <maynab4@gmail.com>
2022-04-19 16:53:26 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+const findMatching = (arr, string) => arr.filter((driver) => {
+    if ( driver === string) {
+       return driver;
+    } if ( driver.toLowerCase() === string.toLowerCase()) {
+      return driver;
+    }
+  })
+;
+console.log(findMatching(drivers, 'Bobby'));
+
+// returns a driver if beginning provided letters match
+
+function fuzzyMatch(arr,str) {
+    const startswith = arr.filter((word) => word.startsWith(str));
+    console.log(startswith)
+    return startswith;
+}
+fuzzyMatch(drivers, "S");
+
+function matchName(arr, str) {
+  const contains = arr.filter((word) => {
+    if ( word.name === str) {
+      return word
+    }
+  });
+  return contains;
+}
+matchName(drivers, 'Bobby')
